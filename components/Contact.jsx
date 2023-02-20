cc6d96d1119546d34d11d48ed5040b2b666aa909
Suvrojyoti Bhuniya <104213139+getcodebySuvro@users.noreply.github.com>
2023-02-20 20:17:30 +0530

    Update Contact.jsx

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -62,7 +62,7 @@ const Contact = () => {
                 
               
               <div data-aos="fade-right" data-aos-delay="1600">
-                <a href="https://drive.google.com/file/d/16yUofNKhHt9oRb8FtXZm47US3EKBL21c/view?usp=drivesdk" target='_blank' rel="noreferrer">
+                <a href="https://drive.google.com/file/d/1EqfYcv7kFgaC-Q0RbeBX8KhqoDF2QfMB/view?usp=sharing" target='_blank' rel="noreferrer">
                   <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer mx-5  hover:text-[#7b28ff] hover:shadow-[#7b28ff] active:scale-[0.9] dark:shadow-none dark:bg-[#7b28ff]/50 dark:hover:bg-[#7b28ff] dark:hover:text-white dark:text-white' >
                     <HiDocumentText/>
                   </div>
