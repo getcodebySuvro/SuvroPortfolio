95ddd4b39cd77489076c79697c254f270099f444
Suvrojyoti Bhuniya <104213139+getcodebySuvro@users.noreply.github.com>
2023-02-20 20:19:41 +0530

    Update Navbar.jsx

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -157,7 +157,7 @@ setTheme("light");
                   </a>
               
               
-                <a href="https://drive.google.com/file/d/16yUofNKhHt9oRb8FtXZm47US3EKBL21c/view?usp=drivesdk" target='_blank' rel="noreferrer">
+                <a href="https://drive.google.com/file/d/1EqfYcv7kFgaC-Q0RbeBX8KhqoDF2QfMB/view?usp=sharing" target='_blank' rel="noreferrer">
                 <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer mx-2 md:mx-3  hover:text-[#7b28ff] hover:shadow-[#7b28ff] active:scale-[0.9]   dark:shadow-none dark:bg-[#7b28ff]/50 dark:hover:bg-[#7b28ff] dark:hover:text-white dark:text-white'>
                   <HiDocumentText/></div>
                 </a>
